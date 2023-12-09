60e25ea5b48c9bf3a500a02e6f3fe25a14c421f3
Amit Kumar <ak47.amit@gmail.com>
2023-12-09 14:38:50 +0000

    Updated logo

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -2,7 +2,7 @@
   <header class="flex items-center py-4 md:py-8">
     <div class="header__logo">
       <nuxt-link to="/">
-        <img :src="logo" alt="Logo" />
+        <img :src="logo" alt="Logo" height="41px" width="39px" />
       </nuxt-link>
     </div>
 
